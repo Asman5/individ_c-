b34ff9ee4a4d1962e35c617209b6084424d33623
Einstein_5 <55026418+Asman5@users.noreply.github.com>
2021-10-03 11:42:15 +0600

    Create 1.cpp

---
+++ lesson_1/1.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // библиотека для ввыода и ввода данных
+
+using namespace std;
+
+int main(){
+    // int a; // double float char string
+    // a = 12.9123456789;
+    // cout << "This is number " << a << endl;
+    // cin >> a;
+    // cout << "This is number " << a;
+    bool b;
+    b = false; // 0
+    b = true; // 1
+    
+
+    return 0; // закончить программу
+}
